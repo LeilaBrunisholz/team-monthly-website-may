d67d53a6bc5501a9ef3499448990bab4d89c1e77
Leila Brunisholz <lhb22920@gmail.com>
2024-05-15 13:47:17 -0400

    PICTURES ADDED

--- script.js
+++ script.js
@@ -3,74 +3,165 @@
 //or do minions and give them all traits
 const teamMembers = [
     {
-        name:'Lionel Messi',
-        age:36,
-        activePlayer:true,
+        name: 'Grouchy',
+        age: 100,
+        activePlayer: true,
         position: 'mid',
         strengths: 'ball control',
         weaknesses: 'heading ability',
-        skills: ['dribbling','shooting','passing'],
+        skills: ['dribbling', 'shooting', 'passing'],
         biography: 'Messi is a really cool dude'
+
     },
     {
-        name:'Lionel Messi',
-        age:36,
-        activePlayer:true,
+        name: 'Nosey',
+        age: 100,
+        activePlayer: true,
         position: 'defender',
         strengths: 'ball control',
         weaknesses: 'heading ability',
-        skills: ['dribbling','shooting','passing'],
+        skills: ['dribbling', 'shooting', 'passing'],
         biography: 'Messi is a really cool dude'
     }, {
-        name:'Lionel Messi',
-        age:36,
-        activePlayer:true,
+        name: 'Hefty',
+        age: 100,
+        activePlayer: true,
         position: 'forward',
         strengths: 'ball control',
         weaknesses: 'heading ability',
-        skills: ['dribbling','shooting','passing'],
+        skills: ['dribbling', 'shooting', 'passing'],
         biography: 'Messi is a really cool dude'
+    },
+    {
+        name: 'Smurfette',
+        age: 100,
+        activePlayer: true,
+        position: 'forward',
+        strengths: 'ball control',
+        weaknesses: 'heading ability',
+        skills: ['dribbling', 'shooting', 'passing'],
+        biography: 'Messi is a really cool dude'
+    },
+    {
+        name: 'Brainy',
+        age: 100,
+        activePlayer: true,
+        position: 'forward',
+        strengths: 'ball control',
+        weaknesses: 'heading ability',
+        skills: ['dribbling', 'shooting', 'passing'],
+        biography: 'Messi is a really cool dude'
+    },
+    {
+        name: 'Poet Smurf',
+        age: 100,
+        activePlayer: true,
+        position: 'forward',
+        strengths: 'ball control',
+        weaknesses: 'heading ability',
+        skills: ['dribbling', 'shooting', 'passing'],
+        biography: 'Messi is a really cool dude'
+    },
+    {
+        name: 'Jokey Smurf',
+        age: 100,
+        activePlayer: true,
+        position: 'forward',
+        strengths: 'ball control',
+        weaknesses: 'heading ability',
+        skills: ['dribbling', 'shooting', 'passing'],
+        biography: 'Messi is a really cool dude'
+    },
+    {
+        name: 'Papa Smurf',
+        age: 542,
+        activePlayer: true,
+        position: 'forward',
+        strengths: 'ball control',
+        weaknesses: 'heading ability',
+        skills: ['dribbling', 'shooting', 'passing'],
+        biography: 'Messi is a really cool dude'
+    },
+    {
+        name: 'Clumsy',
+        age: 100,
+        activePlayer: true,
+        position: 'forward',
+        strengths: 'ball control',
+        weaknesses: 'heading ability',
+        skills: ['dribbling', 'shooting', 'passing'],
+        biography: 'Messi is a really cool dude'
+    },
+    {
+        name: 'Narrator',
+        age: 100,
+        activePlayer: true,
+        position: 'forward',
+        strengths: 'ball control',
+        weaknesses: 'heading ability',
+        skills: ['dribbling', 'shooting', 'passing'],
+        biography: 'Messi is a really cool dude',
+        imageURL:"Narrator.webp"
+
     },
 ]
 
-function generateTeamCards(){
-    const teamCardsContainer=document.getElementById('teamCards')
-teamMembers.forEach(member => {
-    const card = document.createElement('div')
-card.classList.add('col-md-4')
-//style card based on position
-let backgroundColor 
+function generateTeamCards() {
+    const teamCardsContainer = document.getElementById('teamCards')
+    teamMembers.forEach(member => {
+        const card = document.createElement('div')
+        card.classList.add('col-md-4')
+        //style card based on position
+        let positionColor
 
-switch(member.position.toLowerCase()){
-    case 'forward':
-        backgroundColor="red"
-        break
-        case 'mid':
-        backgroundColor="green"
-        break
-            case 'defender':
-                backgroundColor="orange"
+        switch (member.position.toLowerCase()) {
+            case 'forward':
+                positionColor = "red"
                 break
-}
+            case 'mid':
+                positionColor = "green"
+                break
+            case 'defender':
+                positionColor = "orange"
+                break
+        }
 
-card.style.backgroundColor = backgroundColor
+        // Declare ageColor variable before the switch statement
+        let ageColor;
 
+        // Switch statement to assign colors based on age
+        switch (member.age) {
+            case 36:
+                ageColor = "red";
+                break;
+            case 100:
+                ageColor = "green";
+                break;
+            case 542:
+                ageColor = "pink";
+                break;
+            default:
+                ageColor = "red"; 
+        }
 
-card.innerHTML=`
-<div class="card">
-<div class="card-header">
-${member.name}
+        // Now ageColor will have a value based on the switch case, so you can safely use it
+        card.innerHTML = `
+    <div class="card">
+    <img src="${member.imageURL}" class="image fluid">
+        <div class="card-header">
+            ${member.name}
+        </div>
+        <div id="cardBody" class="card-body" style="background-color: ${ageColor};">
+            <p style="color: ${positionColor};"><strong>Position</strong> ${member.position}</p>
+        </div>
     </div>
-    <div id="cardBody" class="card-body" style="background-color:${backgroundColor}">
-    <p><strong>Position</strong> ${member.position}</p>
- </div>
-`
+`;
 
 
-teamCardsContainer.appendChild(card)
+        teamCardsContainer.appendChild(card)
 
 
-});
+    });
 
 }
 

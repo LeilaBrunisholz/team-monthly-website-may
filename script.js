1ac3110632524eb5a8bf4135802c2ecc5b808a07
Leila Brunisholz <lhb22920@gmail.com>
2024-05-16 14:59:15 -0400

    i added character traits

--- script.js
+++ script.js
@@ -6,61 +6,67 @@ const teamMembers = [
         name: 'Grouchy',
         age: 100,
         activePlayer: true,
-        position: 'mid',
-        strengths: 'ball control',
-        weaknesses: 'heading ability',
-        skills: ['dribbling', 'shooting', 'passing'],
-        biography: 'Messi is a really cool dude'
+        position: 'attack',
+        strengths: 'low temper - helps him fight',
+        weaknesses: 'little patience',
+        skills: ['fighting', 'tough'],
+        biography: 'Grouchy is a very angry smurf and puts his anger toward Gargamel',
+        imageURL:"grouchy smurf.webp"
 
     },
     {
         name: 'Nosey',
         age: 100,
         activePlayer: true,
-        position: 'defender',
-        strengths: 'ball control',
-        weaknesses: 'heading ability',
-        skills: ['dribbling', 'shooting', 'passing'],
-        biography: 'Messi is a really cool dude'
+        position: 'spy',
+        strengths: 'listening',
+        weaknesses: 'getting caught spying',
+        skills: ['gossiping', 'spying'],
+        biography: 'Nosey is a really nosey smurf',
+        imageURL:"nosey smurf.webp"
     }, {
         name: 'Hefty',
         age: 100,
         activePlayer: true,
-        position: 'forward',
-        strengths: 'ball control',
-        weaknesses: 'heading ability',
-        skills: ['dribbling', 'shooting', 'passing'],
-        biography: 'Messi is a really cool dude'
+        position: 'attack',
+        strengths: 'fighting',
+        weaknesses: 'sensitive',
+        skills: ['fighting', 'lifting'],
+        biography: 'Hefty is a really strong smurf',
+        imageURL:"hefty.webp"
     },
     {
         name: 'Smurfette',
         age: 100,
         activePlayer: true,
-        position: 'forward',
-        strengths: 'ball control',
-        weaknesses: 'heading ability',
-        skills: ['dribbling', 'shooting', 'passing'],
-        biography: 'Messi is a really cool dude'
+        position: 'distraction',
+        strengths: 'distracting',
+        weaknesses: 'Gargamel hates her specifically',
+        skills: ['karate', 'stalling'],
+        biography: 'Smurfette is a really smart smurf',
+        imageURL:"Smurfette.webp",
     },
     {
         name: 'Brainy',
         age: 100,
         activePlayer: true,
-        position: 'forward',
-        strengths: 'ball control',
-        weaknesses: 'heading ability',
-        skills: ['dribbling', 'shooting', 'passing'],
-        biography: 'Messi is a really cool dude'
+        position: 'brains behind defeating Gargamel',
+        strengths: 'smart',
+        weaknesses: 'fighting ability',
+        skills: ['reading', 'planning'],
+        biography: 'Brainy is a really smart dude',
+        imageURL:"brainy smurf.webp"
     },
     {
         name: 'Poet Smurf',
         age: 100,
         activePlayer: true,
-        position: 'forward',
-        strengths: 'ball control',
-        weaknesses: 'heading ability',
-        skills: ['dribbling', 'shooting', 'passing'],
-        biography: 'Messi is a really cool dude'
+        position: 'make Gargamel distracted with poetry',
+        strengths: 'writing poems',
+        weaknesses: 'strength',
+        skills: ['writing', 'distracting'],
+        biography: 'Messi is a really cool dude',
+        imageURL:"poet.webp"
     },
     {
         name: 'Jokey Smurf',
@@ -70,7 +76,8 @@ const teamMembers = [
         strengths: 'ball control',
         weaknesses: 'heading ability',
         skills: ['dribbling', 'shooting', 'passing'],
-        biography: 'Messi is a really cool dude'
+        biography: 'Messi is a really cool dude',
+        imageURL:"jokey.webp"
     },
     {
         name: 'Papa Smurf',
@@ -80,7 +87,8 @@ const teamMembers = [
         strengths: 'ball control',
         weaknesses: 'heading ability',
         skills: ['dribbling', 'shooting', 'passing'],
-        biography: 'Messi is a really cool dude'
+        biography: 'Messi is a really cool dude',
+        imageURL:"Papa.webp"
     },
     {
         name: 'Clumsy',
@@ -90,7 +98,8 @@ const teamMembers = [
         strengths: 'ball control',
         weaknesses: 'heading ability',
         skills: ['dribbling', 'shooting', 'passing'],
-        biography: 'Messi is a really cool dude'
+        biography: 'Messi is a really cool dude',
+        imageURL:"Clumbsy.webp"
     },
     {
         name: 'Narrator',
@@ -147,12 +156,13 @@ function generateTeamCards() {
         // Now ageColor will have a value based on the switch case, so you can safely use it
         card.innerHTML = `
     <div class="card">
-    <img src="${member.imageURL}" class="image fluid">
+    <img src="${member.imageURL}" class="image-fluid">
         <div class="card-header">
             ${member.name}
         </div>
         <div id="cardBody" class="card-body" style="background-color: ${ageColor};">
             <p style="color: ${positionColor};"><strong>Position</strong> ${member.position}</p>
+            <p style="color: ${positionColor};"><strong>Age:</strong> ${member.age}</p>
         </div>
     </div>
 `;

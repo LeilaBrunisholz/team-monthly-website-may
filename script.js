543acaa88c334461bdf7a614093fbadf446d5884
LeilaBrunisholz <144719570+LeilaBrunisholz@users.noreply.github.com>
2024-05-17 14:09:11 +0000

    giving up

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const teamMembers = [
         age: 100,
         activePlayer: true,
         position: 'attack',
-        strengths: 'low temper - helps him fight',
+        strengths: 'fighting',
         weaknesses: 'little patience',
         skills: ['fighting', 'tough'],
         biography: 'Grouchy is a very angry smurf and puts his anger toward Gargamel',
@@ -19,7 +19,7 @@ const teamMembers = [
         age: 100,
         activePlayer: true,
         position: 'spy',
-        strengths: 'listening',
+        strengths: 'smart',
         weaknesses: 'getting caught spying',
         skills: ['gossiping', 'spying'],
         biography: 'Nosey is a really nosey smurf',
@@ -39,7 +39,7 @@ const teamMembers = [
         name: 'Smurfette',
         age: 100,
         activePlayer: true,
-        position: 'distraction',
+        position: 'distracter',
         strengths: 'distracting',
         weaknesses: 'Gargamel hates her specifically',
         skills: ['karate', 'stalling'],
@@ -50,9 +50,9 @@ const teamMembers = [
         name: 'Brainy',
         age: 100,
         activePlayer: true,
-        position: 'brains behind defeating Gargamel',
+        position: 'brains',
         strengths: 'smart',
-        weaknesses: 'fighting ability',
+        weaknesses: 'fighting',
         skills: ['reading', 'planning'],
         biography: 'Brainy is a really smart dude',
         imageURL:"brainy smurf.webp"
@@ -61,8 +61,8 @@ const teamMembers = [
         name: 'Poet Smurf',
         age: 100,
         activePlayer: true,
-        position: 'make Gargamel distracted with poetry',
-        strengths: 'writing poems',
+        position: 'distracter',
+        strengths: 'communicating',
         weaknesses: 'strength',
         skills: ['writing', 'distracting'],
         biography: 'Messi is a really cool dude',
@@ -72,44 +72,44 @@ const teamMembers = [
         name: 'Jokey Smurf',
         age: 100,
         activePlayer: true,
-        position: 'forward',
-        strengths: 'ball control',
-        weaknesses: 'heading ability',
-        skills: ['dribbling', 'shooting', 'passing'],
-        biography: 'Messi is a really cool dude',
+        position: 'distracter',
+        strengths: 'smart',
+        weaknesses: 'thinking',
+        skills: ['pranking', 'laughing'],
+        biography: 'Jokey is a really funny smurf',
         imageURL:"jokey.webp"
     },
     {
         name: 'Papa Smurf',
         age: 542,
         activePlayer: true,
-        position: 'forward',
-        strengths: 'ball control',
-        weaknesses: 'heading ability',
-        skills: ['dribbling', 'shooting', 'passing'],
-        biography: 'Messi is a really cool dude',
+        position: 'leader',
+        strengths: 'leading',
+        weaknesses: 'fighting',
+        skills: ['being a leader', 'communication'],
+        biography: 'Papa is a really cool smurf',
         imageURL:"Papa.webp"
     },
     {
         name: 'Clumsy',
         age: 100,
         activePlayer: true,
-        position: 'forward',
-        strengths: 'ball control',
-        weaknesses: 'heading ability',
-        skills: ['dribbling', 'shooting', 'passing'],
-        biography: 'Messi is a really cool dude',
+        position: 'back up',
+        strengths: 'cheering',
+        weaknesses: 'clumsiness',
+        skills: ['funny', 'smart'],
+        biography: 'Clumsy is a really clumbsy smurf',
         imageURL:"Clumbsy.webp"
     },
     {
         name: 'Narrator',
         age: 100,
         activePlayer: true,
-        position: 'forward',
-        strengths: 'ball control',
-        weaknesses: 'heading ability',
-        skills: ['dribbling', 'shooting', 'passing'],
-        biography: 'Messi is a really cool dude',
+        position: 'bystander',
+        strengths: 'communicating',
+        weaknesses: 'helping',
+        skills: ['lets everyone know whats happening', 'communication'],
+        biography: 'Narrarator is a really cool smurf',
         imageURL:"Narrator.webp"
 
     },
@@ -124,33 +124,80 @@ function generateTeamCards() {
         let positionColor
 
         switch (member.position.toLowerCase()) {
-            case 'forward':
-                positionColor = "red"
-                break
-            case 'mid':
-                positionColor = "green"
-                break
-            case 'defender':
+            case 'distracter':
                 positionColor = "orange"
                 break
+            case 'attack':
+                positionColor = "pink"
+                break
+            case 'spy':
+                positionColor = "blue"
+                break
+            case 'bystander':
+                positionColor = "red"
+                break
+            case 'back up':
+                    positionColor = "red"
+                    break
+            case 'leader':
+                     positionColor = "red"
+                        break
+            case 'brains':
+                            positionColor = "red"
+                               break
         }
 
+        let strengthsColor
+
+        switch (member.strengths.toLowerCase()) {
+            case 'communicating':
+                strengthsColor = "blue"
+                break
+            case 'leading':
+                strengthsColor = "green"
+                break
+            case 'cheering':
+                strengthsColor = "orange"
+                break
+            case 'cheering':
+                strengthsColor = "orange"
+                break
+            case 'smart':
+                    strengthsColor = "orange"
+                    break
+            case 'distracting':
+                        strengthsColor = "orange"
+                        break
+            case 'fighting':
+                        strengthsColor = "orange"
+                        break
+        }
+
+        let ageColor
+
+        switch (member.strengths.toLowerCase()) {
+            case 100:
+                ageColor = "green"
+                break
+             case 542:
+                    ageColor = "blue"
+                    break
+            
+        }
+        
+
         // Declare ageColor variable before the switch statement
-        let ageColor;
+        let activePlayerColor;
 
         // Switch statement to assign colors based on age
-        switch (member.age) {
-            case 36:
-                ageColor = "red";
+        switch (member.activePlayer) {
+            case 'true':
+                Color = "green";
                 break;
-            case 100:
-                ageColor = "green";
+            case 'false':
+                Color = "red";
                 break;
-            case 542:
-                ageColor = "pink";
-                break;
-            default:
-                ageColor = "red"; 
+            
         }
 
         // Now ageColor will have a value based on the switch case, so you can safely use it
@@ -160,9 +207,12 @@ function generateTeamCards() {
         <div class="card-header">
             ${member.name}
         </div>
-        <div id="cardBody" class="card-body" style="background-color: ${ageColor};">
+        <div id="cardBody" class="card-body" style="background-color: ${member.activePlayer};">
             <p style="color: ${positionColor};"><strong>Position</strong> ${member.position}</p>
-            <p style="color: ${positionColor};"><strong>Age:</strong> ${member.age}</p>
+            <p style="color: ${ageColor};"><strong>Age:</strong> ${member.age}</p>
+            <p style="color: ${strengthsColor};"><strong>Strengths:</strong> ${member.strengths}</p>
+
+
         </div>
     </div>
 `;
